7460029f18103db2f722ed27ad2a74bbe709df94
srimathi-dev <srimathijayu30@gmail.com>
2024-03-12 20:42:09 +0530

    Collection Changes

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -80,6 +80,85 @@ public class Collections
         }
 
 
+        //Hashtable
+        Console.WriteLine("---------------------------HASHTABLE---------------------------------");
+        Hashtable data = new Hashtable();
+        data.Add(1, "Sri");
+        data.Add(2, 30);
+        data.Add(3, 22);
+        data.Add(4, "Sri"); // keys should be unique, values need not be unique
+        data.Add(5, "Mathi");
+        data.Add("6", 100);
+        data.Add("7", null); // Key cannot be null, only value can be null
+
+        Console.WriteLine(data);
+
+        //foreach (var item in data)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+        foreach (DictionaryEntry item in data)
+        {
+            Console.WriteLine($"{item.Key} : {item.Value}");
+        }
+
+        Console.WriteLine("---------------------AFTER REMOVING--------------------------");
+        data.Remove(4);
+        data.Remove("7");
+
+
+        foreach (DictionaryEntry item in data)
+        {
+            Console.WriteLine($"{item.Key} : {item.Value}");
+        }
+
+        Console.WriteLine("---------------------AFTER CLEARING--------------------------");
+
+        data.Clear();
+
+        foreach (DictionaryEntry item in data)
+        {
+            Console.WriteLine($"{item.Key} : {item.Value}");
+        }
+
+        Console.WriteLine("Data is cleared!!!!!!!!");
+
+
+        //Dictionary
+
+        Console.WriteLine("--------------------------DICTIONARY------------------------------");
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        dict.Add(1, 2);
+        dict.Add(2, 3);
+        dict.Add(3, 4);
+        dict.Add(4, 5);
+
+        for(int i = 0;i < dict.Count; i++)
+        {
+            Console.WriteLine($" KEY: {dict.Keys.ElementAt(i)} :  VALUE: {dict.Values.ElementAt(i)}");
+        }
+
+
+        Console.WriteLine("---------------------AFTER REMOVING--------------------------");
+        dict.Remove(4);
+        
+
+        for (int i = 0; i < dict.Count; i++)
+        {
+            Console.WriteLine($" KEY: {dict.Keys.ElementAt(i)} :  VALUE: {dict.Values.ElementAt(i)}");
+        }
+
+        Console.WriteLine("---------------------AFTER CLEARING--------------------------");
+        dict.Clear();
+
+        Console.WriteLine("Cleared DICTIONARY!!!!!!!!!!!!!!!!!!!!!!");
+
+        for (int i = 0; i < dict.Count; i++)
+        {
+            Console.WriteLine($" KEY: {dict.Keys.ElementAt(i)} :  VALUE: {dict.Values.ElementAt(i)}");
+        }
+
 
     }
 }
