8f55f6a5576ceebb3e671fff473b8cee4106bd08
srimathi-dev <srimathijayu30@gmail.com>
2024-03-13 21:34:35 +0530

    OOPS

---
+++ OOPS/Company.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class Company
+    {
+        //private int id = 1001;
+        //private string name = "ZOHO";
+
+        //public void SetId(int idNum)
+        //{
+        //    this.id = idNum;
+        //}
+
+        //public int GetId()
+        //{
+        //    return id;
+        //}
+
+        //public void SetName(string nameCmp)
+        //{
+        //    this.name = nameCmp;
+        //}
+
+        //public string GetName()
+        //{
+        //    return name;
+        //}
+
+
+        //Getter and Setter
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+
+        //Constructor
+        //Default Constructor
+        public Company() 
+        {
+
+        }
+        //Parameterized Constructor
+        public Company(int id, string name, string location)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Location = location;
+        }
+
+        public Company(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine($"ID : {Id}\nName : {Name}");
+        }
+
+        public void DisplayCompanyDetails()
+        {
+            Console.WriteLine($"ID : {Id}\nName : {Name}\nLocation : {Location}");
+        }
+    }
+}
---
+++ OOPS/Program.cs
@@ -0,0 +1,59 @@
+﻿using OOPS;
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Company compObj1 = new Company(1001, "ZOHO", "CHENNAI");
+        compObj1.DisplayCompanyDetails();
+
+        Company compObj2 = new Company(2002, "GOOGLE", "USA");
+        compObj2.DisplayCompanyDetails();
+
+        Company compObj3 = new Company(3003, "APPLE");
+        compObj3.DisplayData();
+
+        Company compObj4 = new Company
+        {
+            Id = 4004,
+            Name = "ATLASSIAN"
+
+        };
+        compObj4.DisplayData();
+
+        Console.WriteLine();
+
+        List<Company> companyList = new List<Company>
+        {
+            new Company
+            {
+                Id = 1001,
+                Name = "ZOHO"
+            },
+
+            new Company
+            {
+                Id = 2002,
+                Name = "GOOGLE"
+            },
+
+            new Company
+            {
+                Id = 3003,
+                Name = "APPLE"
+            },
+
+            new Company
+            {
+                Id = 4004,
+                Name = "ATLASSIAN"
+            }
+        };
+
+        foreach (var item in companyList)
+        {
+            Console.WriteLine($"ID: {item.Id}, Name : {item.Name}");
+        }
+    }
+}
